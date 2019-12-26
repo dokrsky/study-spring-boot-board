c04b4164d4ff2cd614102a7447a1b79e86202860
si.moon <si.moon@gsshop.com>
2019-12-26 14:06:49 +0900

    4장 간단한 게시판 구현하기 완료

--- src/main/java/com/example/board/configuration/DatabaseConfiguration.java
+++ src/main/java/com/example/board/configuration/DatabaseConfiguration.java
@@ -2,7 +2,12 @@ package com.example.board.configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,6 +17,10 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource(value = "classpath:/application.properties")
 public class DatabaseConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     @ConfigurationProperties(prefix="spring.datasource.hikari")
     public HikariConfig hikariConfig() {
@@ -23,5 +32,26 @@ public class DatabaseConfiguration {
         DataSource dataSource = new HikariDataSource(hikariConfig());
         System.out.println(dataSource.toString());
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResource("classpath:/mapper/sql-board.xml"));
+//        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+        sqlSessionFactoryBean.setConfiguration(mybatisConfig());
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfig(){
+        return new org.apache.ibatis.session.Configuration();
     }
 }
